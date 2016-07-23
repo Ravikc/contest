7ac61465036f2ad4c70b5f8b932dea1d168461ef
Ravikc12 <ravi.kc12@gmail.com>
2016-07-23 22:27:42 +0530

    ready to  release

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$(document).click(function (event) {
-    var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");               
-    var _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
-        $navbar.collapse('hide');
-    }
+// $(document).click(function (event) {
+//     var clickover = $(event.target);
+//     var $navbar = $(".navbar-collapse");               
+//     var _opened = $navbar.hasClass("in");
+//     if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+//         $navbar.collapse('hide');
+//     }
+// });
+
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
 });
+
+$('section').on('click',function(){
+$('.collapse').collapse('hide');
+})
